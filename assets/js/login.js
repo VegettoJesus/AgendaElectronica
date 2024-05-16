6caae5128c6fe92eb14b25494b522877c4e69e30
Christian Leon <jesusleonangulo@hotmail.com>
2024-05-15 23:16:50 -0500

    Vista login

---
+++ assets/js/login.js
@@ -0,0 +1,14 @@
+const pass= document.getElementById("pass")
+const icon = document.querySelector(".bx")
+
+icon.addEventListener("click", e=> {
+    if(pass.type === "password"){
+        pass.type = "text"
+        icon.classList.remove('fa-eye')
+        icon.classList.add('fa-eye-slash')
+    }else{
+        pass.type = "password"
+        icon.classList.add('fa-eye')
+        icon.classList.remove('fa-eye-slash')
+    }
+})
